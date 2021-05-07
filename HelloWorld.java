4c9cbd34daa285b229f0da5180b3303df54d97b1
Rathesh Prabakar <ratheshprabakar@gmail.com>
2021-05-07 19:30:03 +0530

    Added HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package javapracticeprograms;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World, Welcome to the world of Java");
+	}
+}
